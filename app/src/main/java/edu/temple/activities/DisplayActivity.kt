13346add457b0265b575027cfe1dc26f23f53875
Elio Berberi <tuo92727@temple.edu>
2026-02-19 11:45:33 -0500

    solution

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,9 +1,12 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 class DisplayActivity : AppCompatActivity() {
 
@@ -14,12 +17,28 @@ class DisplayActivity : AppCompatActivity() {
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
 
+    private val textSizeLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if (result.resultCode == RESULT_OK){
+            val selectedSize = result.data?.getIntExtra("TEXT_SIZE", 22)
+
+            selectedSize?.let {
+                lyricsDisplayTextView.textSize = it.toFloat()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
+
+        textSizeSelectorButton.setOnClickListener {
+            val intent = Intent(this, TextSizeActivity::class.java)
+            textSizeLauncher.launch(intent)
+        }
 
     }
 }
